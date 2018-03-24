b270fe59b05b7874036037f25b29cbcee65556a6
Manthan Thakar <manthant15@gmail.com>
2018-03-24 19:52:51 -0400

    Add A* solution for cut off trees

--- dp/cut_off_trees.py
+++ dp/cut_off_trees.py
@@ -18,7 +18,8 @@ class Solution(object):
             l = self.bfs(forest, source, target)
             if l == -1:
                 return -1
-            path_len += l
+            print "Length from {} -> {}{} = {}".format(source, _, target, l)
+            path_len += l            
             source = target
         return path_len
     
@@ -39,6 +40,34 @@ class Solution(object):
                     visited.add(neighbor)
         # print "Length from {}->{} = {}".format(source, target, length)
         return length
+
+    def a_star(self, forest, source, target):
+        rows, cols = len(forest), len(forest[0])
+        queue, length = [(0, source, 0)], -1
+        heapq.heapify(queue)
+        cost_so_far = {}
+        cost_so_far[source] = 0
+        visited = set()
+        while queue:
+            _, curr, l = heapq.heappop(queue)
+            if curr == target:
+                length = l
+                break
+            for neighbor in self.get_neighbors(curr, rows, cols):
+                neighbor_i, neighbor_j = neighbor[0], neighbor[1]
+                if forest[neighbor_i][neighbor_j] and neighbor not in visited:
+                    neighbor_priority = l + 1 + self.heuristic(neighbor, target)
+                    if neighbor_priority < cost_so_far.get(neighbor, 9999):                        
+                        cost_so_far[neighbor] = neighbor_priority
+                        heapq.heappush(queue, (-neighbor_priority, neighbor, l + 1))
+                        visited.add(neighbor)
+        # print "Length from {}->{} = {}".format(source, target, length)
+        return length
+    
+    def heuristic(self, node, target):
+        x1, y1 = node
+        x2, y2 = target
+        return abs(x1 - x2) + abs(y1 - y2)
 
     def build_heap(self, forest):
         rows, cols = len(forest), len(forest[0])
@@ -67,3 +96,5 @@ if __name__ == '__main__':
         [0, 0, 5],
         [8, 7, 6]
     ])
+    print s.cutOffTree([[54581641, 64080174, 24346381, 69107959], [86374198, 61363882, 68783324, 79706116], [
+                       668150, 92178815, 89819108, 94701471], [83920491, 22724204, 46281641, 47531096], [89078499, 18904913, 25462145, 60813308]])
